16d828ac4217e06240dae05dc6f907128ca63222
pehaw06zz <nhattran22447@gmail.com>
2024-12-11 16:38:40 +0700

    Add files via upload

---
+++ ss3b1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	char name [50]; // khai bao ten nguoi dung
+	printf("nhap ten ban: ");
+	gets(name);
+	printf("xin chao %s\n",name); // in loi chao ten nguoi dung
+	return 0;
+ 
+	
+}
